e0adeeeb4b0e5384f9cf609e56d24120051f15a2
Bruno Machado <brunomachado3818@gmail.com>
2022-10-17 22:38:44 -0400

    Adicionado JavaScript acorrdionList

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent .container-intro");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(
+    ".js-tabcontent .container-intro"
+  );
+  if (tabMenu.length && tabMenu.length) {
+    tabContent[0].classList.add("ativo");
+    tabMenu.forEach((menu, index) => {
+      menu.addEventListener("click", () => {
+        tabContent[index].classList.add("ativo");
+        tabContent.forEach((content, index2) => {
+          if (index !== index2) {
+            content.classList.remove("ativo");
+          }
+        });
+      });
+    });
+  }
+}
 
-if (tabMenu.length && tabMenu.length) {
-  tabContent[0].classList.add("ativo");
-  tabMenu.forEach((menu, index) => {
-    menu.addEventListener("click", () => {
-      tabContent[index].classList.add("ativo");
-      tabContent.forEach((content, index2) => {
-        if (index !== index2) {
-          content.classList.remove("ativo");
+function initAcorrdion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].nextElementSibling.classList.add("ativo");
+    accordionList.forEach((item, index) => {
+      if (index > 0) {
+        item.classList.add("arrow-up");
+      }
+      item.addEventListener("click", () => {
+        item.nextElementSibling.classList.toggle("ativo");
+        if (item.classList.contains("arrow-up")) {
+          item.classList.remove("arrow-up");
+        } else {
+          item.classList.add("arrow-up");
         }
       });
     });
-  });
+  }
 }
+
+initAcorrdion();
+initTabNav();
