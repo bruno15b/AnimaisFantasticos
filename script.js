c7fca932cde57ceeb11f16d72f59b57e92d7e641
Bruno Machado <brunomachado3818@gmail.com>
2022-10-19 16:25:04 -0400

    Adicionado JavaScript animaçao ao scroll

--- script.js
+++ script.js
@@ -53,6 +53,24 @@ function initScrollSuave() {
   });
 }
 
+function initAnimaScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const tela80 = window.innerHeight * 0.8;
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - tela80;
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimaScroll();
 initAcorrdion();
 initTabNav();
 initScrollSuave();
