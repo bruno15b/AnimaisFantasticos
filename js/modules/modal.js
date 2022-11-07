5386875c28360e0b1dac0bf27cfabdeaef970742
Bruno Machado <brunomachado3818@gmail.com>
2022-11-06 22:09:29 -0400

    Adicionado JavaScript modal

---
+++ js/modules/modal.js
@@ -0,0 +1,22 @@
+export default function InitModal() {}
+
+const container = document.querySelector('[data-modal="container"]');
+const abrir = document.querySelector('[data-modal="abrir"]');
+const fechar = document.querySelector('[data-modal="fechar"]');
+
+if (container && abrir && fechar) {
+  function botaoClicado(event) {
+    event.preventDefault();
+    container.classList.toggle("ativo");
+  }
+
+  function foraClick(event) {
+    if (this === event.target) {
+      container.classList.remove("ativo");
+    }
+  }
+
+  container.addEventListener("mousedown", foraClick);
+  abrir.addEventListener("click", botaoClicado);
+  fechar.addEventListener("click", botaoClicado);
+}
--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ import initTabNav from "./modules/tab-nav.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAcorrdion from "./modules/accordion-list.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
+import InitModal from "./modules/modal.js";
 
+InitModal();
 initAnimaScroll();
 initAcorrdion();
 initTabNav();
