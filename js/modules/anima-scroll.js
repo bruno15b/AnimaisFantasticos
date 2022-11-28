0b7a6d730723464e39de91ce2b24d5a9eb52f872
Bruno Machado <brunomachado3818@gmail.com>
2022-11-28 15:10:53 -0300

    animaçao ao scroll refatorado para classe

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,18 +1,28 @@
-export default function initAnimaScroll() {
-  const sections = document.querySelectorAll('[data-anima="scroll"]');
-  const tela80 = window.innerHeight * 0.8;
+export default class AnimaScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.startHeight = window.innerHeight * 0.8;
+    this.animaScroll = this.animaScroll.bind(this);
+  }
 
-  function animaScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - tela80;
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - this.startHeight;
+      console.log(this.startHeight);
       if (sectionTop < 0) {
         section.classList.add("ativo");
       }
     });
   }
 
-  if (sections.length) {
-    animaScroll();
-    window.addEventListener("scroll", animaScroll);
+  addWindowEvent() {
+    window.addEventListener("scroll", this.animaScroll);
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.animaScroll();
+      this.addWindowEvent();
+    }
   }
 }
