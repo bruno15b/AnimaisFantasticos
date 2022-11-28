2f66e05cf827eed00451db48c0e1816216505cef
Bruno Machado <brunomachado3818@gmail.com>
2022-11-28 16:05:26 -0300

    animaçao ao scroll refeita

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -2,27 +2,44 @@ export default class AnimaScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.startHeight = window.innerHeight * 0.8;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.class = "ativo";
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - this.startHeight;
-      console.log(this.startHeight);
-      if (sectionTop < 0) {
-        section.classList.add("ativo");
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offSet = section.offsetTop;
+      return {
+        element: section,
+        offSet: offSet - this.startHeight,
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offSet) {
+        item.element.classList.add(this.class);
+      } else if (item.element.classList.contains(this.class)) {
+        item.element.classList.remove(this.class);
       }
     });
   }
 
   addWindowEvent() {
-    window.addEventListener("scroll", this.animaScroll);
+    window.addEventListener("scroll", this.checkDistance);
   }
 
   init() {
     if (this.sections.length) {
-      this.animaScroll();
+      this.getDistance();
+      this.checkDistance();
       this.addWindowEvent();
     }
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
